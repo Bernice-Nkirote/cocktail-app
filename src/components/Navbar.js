158b64b4d777fbd1fda553c65e8f7207709ccbfd
Bernice-Nkirote <bernicemwirigi@gmail.com>
2022-08-03 13:36:57 +0300

    first commit for cocktail app

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <div className='nav-center'>
+        <span className='logo'>
+          <Link to='/'>
+            <h2>Cocktail Bar</h2>
+          </Link>
+        </span>
+        <ul className='nav-links'>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About Us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio neque
+        minima incidunt in id, doloribus tenetur nihil? Sit, mollitia rem
+        cupiditate exercitationem nesciunt animi nihil, veniam totam voluptatem
+        aliquid error alias dolorem molestias saepe neque quis expedita incidunt
+        voluptates pariatur!
+      </p>
+    </section>
+  );
+};
+
+export default About;
